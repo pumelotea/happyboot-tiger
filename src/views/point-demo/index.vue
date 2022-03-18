f01d88061aa6fd6e6b53182f57f291567063d2b6
pumelotea <zf_722@yeah.net>
2022-03-18 11:14:28 +0800

    feat: 新增权限点演示

--- src/views/point-demo/index.vue
+++ src/views/point-demo/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {NLayout ,NLayoutHeader,NLayoutContent,NPageHeader,NCode,NButton,NSpace} from 'naive-ui';
+import {NLayout,NLayoutContent,NAlert,NCode,NButton,NSpace,NCard} from 'naive-ui';
 import HbAdminPageLayout from "../../components/HbAdminPageLayout.vue";
 import hljs from 'highlight.js/lib/core'
 import javascript from 'highlight.js/lib/languages/javascript'
@@ -49,19 +49,29 @@ const htmlCode = `
   <hb-admin-page-layout>
     <n-layout style="padding:20px">
       <n-layout-content>
-        <h2>权限点的演示</h2>
-        <n-space align="center">
-          <n-button type="info" v-point="'add'">被控制的按钮</n-button>
-          <n-button type="warning" v-point="'cancel'">未被控制的按钮</n-button>
-          <div>
-            <div class="no-key-btn">控制点未被对应的按钮</div>
-            <n-button type="warning" v-point="'no-key'">这里的按钮被隐藏了</n-button>
-          </div>
+        <n-space vertical>
+          <h2>权限点的演示</h2>
+          <n-space align="center">
+            <n-button type="info" v-point="'add'">被控制的按钮</n-button>
+            <n-button type="warning" v-point="'cancel'">未被控制的按钮</n-button>
+            <div>
+              <div class="no-key-btn">控制点未被对应的按钮</div>
+              <n-button type="warning" v-point="'no-key'">这里的按钮被隐藏了</n-button>
+            </div>
+          </n-space>
+          <n-alert type="info">
+            Vue模板中使用`v-point`，路由节点中的`permissionKey`属性和指令中配置的值相同，则显示，否则不显示<br/>如以下代码
+          </n-alert>
+          <n-card>
+            <n-code :code="htmlCode" language="xml" :hljs="hljs"></n-code>
+          </n-card>
+          <n-alert type="info">
+            权限点配置在路由节点的子节点中，<br/>如以下代码
+          </n-alert>
+         <n-card>
+           <n-code :code="data" language="JavaScript" :hljs="hljs"></n-code>
+         </n-card>
         </n-space>
-        <h4>Vue模板中使用`v-point`，路由节点中的`permissionKey`属性和指令中配置的值相同，则显示，否则不显示<br/>如以下代码</h4>
-        <n-code :code="htmlCode" language="xml" :hljs="hljs"></n-code>
-        <h4>权限点配置在路由节点的子节点中，<br/>如以下代码</h4>
-        <n-code :code="data" language="JavaScript" :hljs="hljs"></n-code>
       </n-layout-content>
     </n-layout>
   </hb-admin-page-layout>
