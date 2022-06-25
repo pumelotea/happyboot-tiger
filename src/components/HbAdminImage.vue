fce1ae6b5cf7f560df5b910ebd092dfda8d2049e
lichunhui <380060615@qq.com>
2022-06-25 14:42:12 +0800

    add: 图片组件

---
+++ src/components/HbAdminImage.vue
@@ -0,0 +1,174 @@
+<script setup>
+import { NIcon, useThemeVars } from 'naive-ui'
+import HbAdminImagePreview from "@/components/HbAdminImagePreview"
+const themeVars = useThemeVars()
+/**
+ * 功能说明：
+ * 设置宽高后，图片将按比例缩放到完整显示较小的边，长的边会进行裁剪
+ * */
+
+import {nextTick, onMounted, ref} from "vue";
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: ''
+  },
+  prevSrc: {
+    type: String,
+    default: ''
+  },
+  height: {
+    type: Number,
+    default: null
+  },
+  width: {
+    type: Number,
+    default: null
+  },
+  radius: {
+    type: String,
+    default: '0px'
+  },
+  isPreview: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const HbImgWrap = ref()
+const HbImg = ref()
+let imgHeight = 0
+let imgWidth = 0
+
+function calcImg() {
+  //图片宽高比
+  let WHPercent = imgWidth / imgHeight
+  //图片高宽比
+  let HWPercent = imgHeight / imgWidth
+  //容器高度
+  let divHeight = imgHeight
+  if (props.height) {
+    divHeight = props.height
+  }
+  //容器宽度
+  let divWidth = imgWidth
+  if (props.width) {
+    divWidth = props.width
+  }
+  //设置容器高度
+  HbImgWrap.value.style.height = divHeight + 'px'
+  //设置容器宽度
+  HbImgWrap.value.style.width = divWidth + 'px'
+
+  console.log(divHeight + ';' + divWidth)
+  //容器宽高比
+  let divPercent = divWidth / divHeight
+  if (imgWidth > divWidth && imgHeight < divHeight) { //图片宽比容器大 高比容器小
+    HbImg.value.style.setProperty('height', divHeight + 'px', 'important')
+    HbImg.value.style.setProperty('width', divHeight * WHPercent + 'px', 'important')
+  }else if (imgWidth < divWidth && imgHeight > divHeight) { //图片高比容器大 宽比容器小
+    HbImg.value.style.setProperty('height', divWidth * HWPercent + 'px', 'important')
+    HbImg.value.style.setProperty('width', divWidth + 'px', 'important')
+  }else {  // 图片宽高都大于容器  或  图片宽高都小于容器
+    if (WHPercent > 1) {
+      if(WHPercent > divPercent){
+        HbImg.value.style.setProperty('height', divHeight + 'px', 'important')
+        HbImg.value.style.setProperty('width', divHeight * WHPercent + 'px', 'important')
+      }else {
+        HbImg.value.style.setProperty('height', divWidth * HWPercent + 'px', 'important')
+        HbImg.value.style.setProperty('width', divWidth + 'px', 'important')
+      }
+    }else if (WHPercent < 1) {
+      if(divPercent >= 1){
+        HbImg.value.style.setProperty('height', divWidth * HWPercent + 'px', 'important')
+        HbImg.value.style.setProperty('width', divWidth + 'px', 'important')
+      }else {
+        HbImg.value.style.setProperty('height', divHeight + 'px', 'important')
+        HbImg.value.style.setProperty('width', divHeight * WHPercent + 'px', 'important')
+      }
+    }else {
+      if(divPercent >= 1){
+        HbImg.value.style.setProperty('height', divWidth * HWPercent + 'px', 'important')
+        HbImg.value.style.setProperty('width', divWidth + 'px', 'important')
+      }else {
+        HbImg.value.style.setProperty('height', divHeight + 'px', 'important')
+        HbImg.value.style.setProperty('width', divHeight * WHPercent + 'px', 'important')
+      }
+    }
+  }
+}
+
+const isShow = ref(false)
+
+onMounted(() => {
+  let image = new Image()
+  image.src = props.src
+  if(image.complete){
+    imgHeight = HbImg.value.naturalHeight
+    imgWidth = HbImg.value.naturalWidth
+    calcImg()
+  }else {
+    HbImg.value.src = 'images/no-img.png'
+    nextTick(() => {
+      image.onload = () => {
+        HbImg.value.src = props.src
+        imgHeight = HbImg.value.offsetHeight
+        imgWidth = HbImg.value.offsetWidth
+        calcImg()
+      }
+      image.onerror = () => {
+        HbImg.value.src = 'images/no-img.png'
+        imgHeight = HbImg.value.offsetHeight
+        imgWidth = HbImg.value.offsetWidth
+        calcImg()
+      }
+    })
+  }
+})
+
+</script>
+
+<template>
+  <div ref="HbImgWrap" class="hb-img-wrap" :style="`border-radius: ${props.radius};`">
+    <img ref="HbImg" class="hb-img" :src="src">
+    <div class="hb-img-prev" v-show="props.isPreview">
+      <n-icon style="cursor: pointer" @click="isShow = true">
+        <i class="ri-eye-line"></i>
+      </n-icon>
+    </div>
+  </div>
+  <hb-admin-image-preview v-model:value="isShow" :list="[props.prevSrc || props.src]"/>
+</template>
+
+<style scoped>
+.hb-img-wrap {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.hb-img-wrap:hover .hb-img-prev{
+  display: flex;
+}
+.hb-img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding:0; margin:0;
+}
+.hb-img-prev{
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0,0,0,0.5);
+  color: v-bind(themeVars.textColor3);
+  font-size: 28px;
+  display: none;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/image-demo/index.vue
@@ -0,0 +1,69 @@
+<script setup>
+import {NLayout,NLayoutContent,NAlert,NButton,NSpace,NCard,NModal, NTimeline, NTimelineItem,NIcon,useMessage} from 'naive-ui';
+import HbAdminPageLayout from "@/components/HbAdminPageLayout.vue"
+import HbAdminImage from "@/components/HbAdminImage.vue";
+
+</script>
+
+<template>
+  <hb-admin-page-layout>
+    <n-layout style="padding:20px">
+      <n-layout-content>
+        <n-space vertical>
+          <h2>图片组件</h2>
+          <n-alert type="info">
+            设置宽高后，图片将按比例缩放到完整显示较小的边，长的边会进行裁剪(不设置宽高时，为图片原始宽高)
+          </n-alert>
+          <n-card>
+            <div class="demo-image-div">
+              <hb-admin-image
+                  class="demo-image"
+                  src="https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
+                  prev-src="https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg"
+                  is-preview
+              />
+              <div class="demo-image-text">
+                原图（800px * 533px） ps:可以自定义预览时的图片地址
+              </div>
+              <hb-admin-image
+                  class="demo-image"
+                  :height="400"
+                  :width="300"
+                  radius="10px"
+                  is-preview
+                  src="https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
+              />
+              <div class="demo-image-text">
+                窄图（300px * 400px） ps:可以加圆角
+              </div>
+              <hb-admin-image
+                  class="demo-image"
+                  :height="200"
+                  :width="800"
+                  src="https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
+              />
+              <div class="demo-image-text">
+                扁图（800px * 200px） ps:默认情况下不开启预览功能
+              </div>
+            </div>
+          </n-card>
+        </n-space>
+      </n-layout-content>
+    </n-layout>
+  </hb-admin-page-layout>
+</template>
+
+<style scoped>
+.demo-image-div{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.demo-image-text{
+  margin: 15px 0;
+}
+.demo-image{
+  box-shadow: 0 5px 10px 0 rgba(0,0,0,0.5);
+}
+</style>
