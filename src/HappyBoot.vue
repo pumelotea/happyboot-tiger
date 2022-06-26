ae56a3158046a5e7778dc4034419775063eade9e
pumelotea <zf_722@yeah.net>
2022-06-26 17:47:51 +0800

    feat: 美化滚动条样式跟随主题

--- src/HappyBoot.vue
+++ src/HappyBoot.vue
@@ -1,11 +1,32 @@
 <script setup>
 import HbAdminDevTool from "@/components/dev/HbAdminDevTool.vue";
-import { useMessage } from 'naive-ui'
+import { useMessage,useThemeVars } from 'naive-ui'
 import HbAdminLoading from "@/components/HbAdminLoading.vue";
 window.$message = useMessage()
+const vars = useThemeVars()
 </script>
 <template>
   <router-view/>
   <hb-admin-dev-tool/>
   <hb-admin-loading/>
 </template>
+
+<style>
+::-webkit-scrollbar {
+  width: v-bind(vars.scrollbarWidth);
+  height: v-bind(vars.scrollbarHeight);
+}
+/*定义滚动条轨道 内阴影+圆角*/
+::-webkit-scrollbar-track {
+  border-radius: v-bind(vars.scrollbarBorderRadius);
+}
+/*定义滑块 内阴影+圆角*/
+::-webkit-scrollbar-thumb {
+  border-radius: v-bind(vars.scrollbarBorderRadius);
+  background-color: v-bind(vars.scrollbarColor);
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: v-bind(vars.scrollbarColorHover);
+}
+</style>
