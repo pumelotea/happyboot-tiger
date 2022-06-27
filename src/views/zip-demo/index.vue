5154a6876137b8af1c687875b110cd7df0a0d998
pumelotea <zf_722@yeah.net>
2022-06-27 16:11:59 +0800

    feat: 优化zip demo

--- src/views/zip-demo/index.vue
+++ src/views/zip-demo/index.vue
@@ -1,7 +1,11 @@
 <script setup>
-import {NLayout, NLayoutContent, NAlert, NButton, NSpace, NCard} from 'naive-ui';
+import {NLayout, NLayoutContent, NAlert, NSpace} from 'naive-ui'
 import HbAdminPageLayout from "@/components/HbAdminPageLayout.vue"
+import HbAdminDemoCard from "@/components/HbAdminDemoCard"
+import Demo from "./demo"
 
+const code = `<script setup>
+import {NButton, NSpace} from 'naive-ui';
 import jszip from 'jszip'
 
 async function makeZip(){
@@ -13,11 +17,23 @@ async function makeZip(){
   const a = document.createElement("a");
   const url = window.URL.createObjectURL(blob);
   a.href = url;
-  a.download = `data.zip`;
+  a.download = \`data.zip\`;
   a.click();
   window.URL.revokeObjectURL(url)
 }
-
+<\/script>
+<template>
+  示例文件目录结构
+  <pre>
+  -- hello.txt
+  -- package
+  ---- hello2.txt
+</pre>
+  <n-space justify="flex-end" style="margin-top: 10px">
+    <n-button type="info" @click="makeZip">生成压缩包</n-button>
+  </n-space>
+<\/template>
+`
 </script>
 <template>
   <hb-admin-page-layout>
@@ -28,19 +44,9 @@ async function makeZip(){
           <n-alert type="info">
             基于jszip实现
           </n-alert>
-          <n-card>
-            示例文件目录结构
-<pre>
-  -- hello.txt
-  -- package
-  ---- hello2.txt
-</pre>
-            <template #footer>
-              <n-space justify="flex-end">
-                <n-button type="info" @click="makeZip">生成压缩包</n-button>
-              </n-space>
-            </template>
-          </n-card>
+          <hb-admin-demo-card title="基本用法" :code="code">
+            <demo/>
+          </hb-admin-demo-card>
         </n-space>
       </n-layout-content>
     </n-layout>
