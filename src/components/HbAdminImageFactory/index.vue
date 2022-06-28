f66ecac31e7a157c3438403a639222611acc85a4
lichunhui <380060615@qq.com>
2022-06-28 09:15:05 +0800

    feat：图片工厂组件

---
+++ src/components/HbAdminImageFactory/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HbWaterMark from './components/HbWaterMark.vue'
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'watermark'
+  }
+})
+</script>
+
+<template>
+  <hb-water-mark v-if="props.type === 'watermark'" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/image-factory-demo/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {NLayout,NLayoutContent,NAlert,NButton,NSpace,NCard,NModal, NTimeline, NTimelineItem,NIcon,useMessage} from 'naive-ui';
+import HbAdminPageLayout from "@/components/HbAdminPageLayout.vue"
+import HbAdminImageFactory from "@/components/HbAdminImageFactory/index.vue";
+
+</script>
+
+<template>
+  <hb-admin-page-layout>
+    <n-layout style="padding:20px">
+      <n-layout-content>
+        <n-space vertical>
+          <h2>图片工厂组件</h2>
+          <n-alert type="info">
+            此组件可以上传或者传入一个图片地址进行对图片的一些二次加工处理。例如：调整图片大小、设置图片质量、添加图片水印等。
+          </n-alert>
+          <n-card>
+            <div class="demo-image-factory">
+              <hb-admin-image-factory type="watermark" />
+            </div>
+          </n-card>
+        </n-space>
+      </n-layout-content>
+    </n-layout>
+  </hb-admin-page-layout>
+</template>
+
+<style scoped>
+.demo-image-factory{
+  height: 700px;
+  width: 100%;
+}
+</style>
