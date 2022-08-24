ba5be92697f579c2815ad8240a1802237bd4dcf2
lichunhui <380060615@qq.com>
2022-08-24 09:46:47 +0800

    fix: 可调整容器

--- src/components/HbAdminAdjustableContainer.vue
+++ src/components/HbAdminAdjustableContainer.vue
@@ -5,20 +5,21 @@ import {onMounted, ref, watch} from "vue";
 const props = defineProps({
   height: {
     type: Number,
-    default: 250
+    default: 200
   },
   width: {
     type: Number,
-    default: 500
+    default: 200
   }
 })
 
-const cHeight = ref(250)
-const cWidth = ref(500)
+const cHeight = ref(0)
+const cWidth = ref(0)
 const iHeight = ref(250)
 const iWidth = ref(500)
 const equalRatio = ref(false)
 let canDrag = false    //是否允许拖拽
+const showDrag = ref(false)    //是否显示拖动框
 let mouseDownState = false    //鼠标是否按下
 let moveTemp = [0, 0]    //上一次拖拽位置的缓存
 const ACWrap = ref()
@@ -55,28 +56,56 @@ function handleWidthChange() {
 }
 
 onMounted(() => {
-  cHeight.value = props.height
-  cWidth.value = props.width
-  iHeight.value = props.height
-  iWidth.value = props.width
+  const offsetHeight = AdjustableContainer.value.offsetHeight
+  const offsetWidth = AdjustableContainer.value.offsetWidth
+  console.log(offsetWidth)
+  console.log(offsetHeight)
+  if (offsetHeight === 0 || offsetWidth === 0) {
+    cHeight.value = props.height
+    cWidth.value = props.width
+    iHeight.value = props.height
+    iWidth.value = props.width
+  } else {
+    cHeight.value = offsetHeight
+    cWidth.value = offsetWidth
+    iHeight.value = offsetHeight
+    iWidth.value = offsetWidth
+  }
   AdjustableContainer.value.style.height = cHeight.value + 'px'
   AdjustableContainer.value.style.width = cWidth.value + 'px'
 
   ACWrap.value.addEventListener('mousemove', e => {
     const diffX = ACWrap.value.offsetWidth - e.offsetX
     const diffY = ACWrap.value.offsetHeight - e.offsetY
-    if (diffX <= 20 && diffX >= -20 && diffY <= 20 && diffY >= -20) {
+    if (diffX <= 10 && diffX > 1 && diffY <= 10 && diffY > 1) {
       ACWrap.value.style.cursor = 'nw-resize'
       canDrag = true
     } else {
-      ACWrap.value.style.cursor = 'default'
+      if (!mouseDownState) {
+        ACWrap.value.style.cursor = 'default'
+        canDrag = false
+      }
+    }
+    showDrag.value = true
+    ACWrap.value.style.border = '2px solid #2080F0'
+  })
+  ACWrap.value.addEventListener('mouseenter', e => {
+    showDrag.value = true
+    ACWrap.value.style.border = '2px solid #2080F0'
+  })
+  ACWrap.value.addEventListener('mouseleave', e => {
+    if (!mouseDownState) {
+      showDrag.value = false
+      ACWrap.value.style.border = '2px solid #fff'
     }
   })
 
   ACWrap.value.addEventListener('mousedown', e => {
-    mouseDownState = true
-    moveTemp = [e.offsetX, e.offsetY]
-
+    if (canDrag) {
+      mouseDownState = true
+      moveTemp = [e.offsetX, e.offsetY]
+      ACWrap.value.style.userSelect = 'none'
+    }
   })
 
   document.body.addEventListener('mousemove', e => {
@@ -89,18 +118,26 @@ onMounted(() => {
         cWidth.value = cWidth.value + diffX
         cHeight.value =  cWidth.value / ratio
       } else {
-        cHeight.value = cHeight.value + diffY
-        cWidth.value = cWidth.value + diffX
+        const dHeight = cHeight.value + diffY
+        cHeight.value = dHeight > 10 ? dHeight : 10
+        const dWidth = cWidth.value + diffX
+        cWidth.value = dWidth > 10 ? dWidth : 10
       }
-
+      document.body.style.userSelect = 'none'
       AdjustableContainer.value.style.height = cHeight.value + 'px'
       AdjustableContainer.value.style.width = cWidth.value + 'px'
     }
   })
 
   document.body.addEventListener('mouseup', e => {
+    console.log('mouseup')
     canDrag = false
     mouseDownState = false
+    ACWrap.value.style.cursor = 'default'
+    ACWrap.value.style.userSelect = 'default'
+    ACWrap.value.style.border = '2px solid #fff'
+    document.body.style.userSelect = 'default'
+    showDrag.value = false
   })
 })
 
@@ -111,7 +148,8 @@ onMounted(() => {
     <div ref="AdjustableContainer" class="hb-ac-content">
       <slot/>
     </div>
-    <div class="hb-ac-bar">
+    <div class="hb-ac-drag-block" v-show="showDrag"></div>
+    <div class="hb-ac-bar" v-show="false">
       <div>
         <div>高度(px)</div>
         <div>
@@ -131,10 +169,14 @@ onMounted(() => {
 
 <style scoped>
 .hb-ac-wrap{
+  max-width: 100%;
   width:auto;
   display:inline-block !important;
   padding: 5px;
-  border: 1px solid #aaa;
+  border: 2px solid #fff;
+  position: relative;
+}
+.hb-ac-content{
   position: relative;
 }
 .hb-ac-bar{
@@ -147,8 +189,13 @@ onMounted(() => {
 .hb-ac-input{
   width: 50px;
 }
-
-.hb-ac-content{
-  position: relative;
+.hb-ac-drag-block{
+  height: 6px;
+  width: 6px;
+  position: absolute;
+  right: -1px;
+  bottom: -1px;
+  pointer-events: none;
+  border: 1px solid #aaaaaa;
 }
 </style>
