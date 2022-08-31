12c77ea48c059e29412c9f37d94f50e31722cb23
lichunhui <380060615@qq.com>
2022-08-31 09:13:53 +0800

    upd:数学公式弹框样式优化

--- src/components/tiptap/components/HbTiptapMath.vue
+++ src/components/tiptap/components/HbTiptapMath.vue
@@ -2,117 +2,25 @@
 import {NModal, NButton, NPopover, NGrid, NGridItem, NSpace} from 'naive-ui'
 import 'katex/dist/katex.css'
 import katex from 'katex'
-import {ref, watch} from "vue";
-import {useThemeVars} from 'naive-ui'
+import {nextTick, ref, watch} from "vue";
+import HbTiptapMathContent from "./HbTiptapMathContent"
 
-const vars = useThemeVars()
+const HTMC = ref(null)
 
 const showModal = ref(false)
 
-const tex = ref('')
-
-const TEXAREA = ref(null)
-
-function addTex(val) {
-  const indexStart = TEXAREA.value.selectionStart
-  const indexEnd = TEXAREA.value.selectionEnd
-  tex.value = tex.value.substr(0, indexStart) + val + tex.value.substr(indexEnd, tex.value.length)
-}
-
-watch(tex, () => {
-  init()
-})
-
-const box = ref(null)
-
-function init() {
-  if (tex.value) {
-    katex.render(tex.value, box.value, {
-      throwOnError: false
-    })
-  }
-}
-
-const letters = ref({
-  'α': '\\alpha',
-  'β': '\\beta',
-  'γ': '\\gamma',
-  'δ': '\\delta',
-  'ϵ': '\\epsilon',
-  'ζ': '\\zeta',
-  'η': '\\eta',
-  'θ': '\\theta',
-  'ι': '\\iota',
-  'κ': '\\kappa',
-  'λ': '\\lambda',
-  'μ': '\\mu',
-  'ν': '\\nu',
-  'ξ': '\\xi',
-  'ο': '\\omicron',
-  'π': '\\pi',
-  'ρ': '\\rho',
-  'σ': '\\sigma',
-  'τ': '\\tau',
-  'υ': '\\upsilon',
-  'ϕ': '\\phi',
-  'χ': '\\chi',
-  'ψ': '\\psi',
-  'ω': '\\omega',
-  'ε': '\\varepsilon',
-  'ϰ': '\\varkappa',
-  'ϑ': '\\vartheta',
-  'ϖ': '\\varpi',
-  'ϱ': '\\varrho',
-  'ς': '\\varsigma',
-  'φ': '\\varphi',
-  'ϝ': '\\digamma',
-})
-const logic = ref({
-  '×': '\\times',
-  '÷': '\\div',
-  '≈': '\\approx',
-  '≪': '\\ll',
-  '≫': '\\gg',
-  '≂': '\\eqsim',
-  '⪖': '\\eqslantgtr',
-  '⪕': '\\eqslantless',
-  '⊕': '\\oplus',
-  '±': '\\pm',
-  '∙': '\\bullet',
-  '∀': '\\forall',
-  '∁': '\\complement',
-  '∴': '\\therefore',
-  '∅': '\\varnothing',
-  '∃': '\\exists',
-  '⊂': '\\subset',
-  '∵': '\\because',
-  '⊃': '\\supset',
-  '↦': '\\mapsto',
-  '∄': '\\nexists',
-  '∣': '\\mid',
-  '→': '\\to',
-  '⟹': '\\implies',
-  '∈': '\\in',
-  '∧': '\\land',
-  '←': '\\gets',
-  '⟸': '\\impliedby',
-  '∨': '\\lor',
-  '↔': '\\leftrightarrow',
-  '⟺': '\\iff',
-  '∋': '\\ni',
-  '¬': '\\neg',
-})
-
 function open(val = '') {
   showModal.value = true
-  tex.value = val
+  nextTick(() => {
+    HTMC.value.setTex(val)
+  })
 }
 
 const emit = defineEmits(['ok'])
 
 function onOk() {
   showModal.value = false
-  emit('ok', tex.value)
+  emit('ok', HTMC.value.getTex())
 }
 
 function onCancel() {
@@ -127,35 +35,7 @@ defineExpose({open})
     <template #header>
       <div>插入公式</div>
     </template>
-    <div style="margin-bottom: 10px">
-      <n-popover>
-        <template #trigger>
-          <n-button size="small" type="primary" secondary>希腊字母</n-button>
-        </template>
-        <n-grid :y-gap="5" :cols="12">
-          <n-grid-item class="item-hover" v-for="(val, key, i) in letters" @click="addTex(val)">
-            {{ key }}
-          </n-grid-item>
-        </n-grid>
-      </n-popover>
-      <n-popover>
-        <template #trigger>
-          <n-button size="small" type="primary"  secondary style="margin-left: 10px">逻辑符号</n-button>
-        </template>
-        <n-grid :y-gap="5" :cols="12">
-          <n-grid-item class="item-hover" v-for="(val, key, i) in logic" @click="addTex(val)">
-            {{ key }}
-          </n-grid-item>
-        </n-grid>
-      </n-popover>
-    </div>
-    <div class="texbox">
-      <textarea ref="TEXAREA" v-model="tex" class="texarea" placeholder="输入TeX公式"/>
-    </div>
-    <div class="preview-title">预览</div>
-    <div class="preview">
-      <p ref="box"></p>
-    </div>
+    <hb-tiptap-math-content ref="HTMC" @add="addTex"/>
     <template #footer>
       <n-space justify="end">
         <n-button @click="onCancel">取消</n-button>
@@ -164,55 +44,5 @@ defineExpose({open})
     </template>
   </n-modal>
 </template>
-<style scoped>
-.item-hover {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 3px;
-  cursor: pointer;
-  font-size: 17px;
-  color: var(--n-text-color);
-  width: 30px;
-  height: 30px;
-}
 
-.item-hover:hover {
-  background: v-bind(vars.primaryColor);
-}
-
-.texbox {
-  box-sizing: border-box;
-  border: 1px solid var(--n-border-color);
-  border-radius: 3px;
-  height: 200px;
-  overflow: hidden;
-}
-
-.texarea {
-  width: 100%;
-  height: 100%;
-  border: none;
-  outline: none;
-  resize: none;
-  background: var(--n-color);
-  padding: 10px;
-  box-sizing: border-box;
-  color: var(--n-text-color);
-}
-
-.preview-title{
-  font-size: 12px;
-  margin-top: 20px;
-  color: var(--n-text-color);
-}
-
-.preview {
-  padding: 10px;
-  box-sizing: border-box;
-  border: 1px solid var(--n-border-color);
-  border-radius: 3px;
-  min-height: 80px;
-}
-</style>
 
---
+++ src/components/tiptap/components/HbTiptapMathContent.vue
@@ -0,0 +1,200 @@
+<script setup>
+import {useThemeVars, NButton, NPopover, NGrid, NGridItem, NSpace} from 'naive-ui'
+import {ref, watch} from "vue";
+import katex from "katex";
+
+const themeVars = useThemeVars()
+
+const tex = ref('')
+
+const TEXAREA = ref(null)
+
+function addTex(val) {
+  const indexStart = TEXAREA.value.selectionStart
+  const indexEnd = TEXAREA.value.selectionEnd
+  tex.value = tex.value.substr(0, indexStart) + val + tex.value.substr(indexEnd, tex.value.length)
+}
+
+watch(tex, () => {
+  init()
+})
+
+const box = ref(null)
+
+function init() {
+  if (tex.value) {
+    katex.render(tex.value, box.value, {
+      throwOnError: false
+    })
+  }
+}
+
+const letters = ref({
+  'α': '\\alpha',
+  'β': '\\beta',
+  'γ': '\\gamma',
+  'δ': '\\delta',
+  'ϵ': '\\epsilon',
+  'ζ': '\\zeta',
+  'η': '\\eta',
+  'θ': '\\theta',
+  'ι': '\\iota',
+  'κ': '\\kappa',
+  'λ': '\\lambda',
+  'μ': '\\mu',
+  'ν': '\\nu',
+  'ξ': '\\xi',
+  'ο': '\\omicron',
+  'π': '\\pi',
+  'ρ': '\\rho',
+  'σ': '\\sigma',
+  'τ': '\\tau',
+  'υ': '\\upsilon',
+  'ϕ': '\\phi',
+  'χ': '\\chi',
+  'ψ': '\\psi',
+  'ω': '\\omega',
+  'ε': '\\varepsilon',
+  'ϰ': '\\varkappa',
+  'ϑ': '\\vartheta',
+  'ϖ': '\\varpi',
+  'ϱ': '\\varrho',
+  'ς': '\\varsigma',
+  'φ': '\\varphi',
+  'ϝ': '\\digamma',
+})
+const logic = ref({
+  '×': '\\times',
+  '÷': '\\div',
+  '≈': '\\approx',
+  '≪': '\\ll',
+  '≫': '\\gg',
+  '≂': '\\eqsim',
+  '⪖': '\\eqslantgtr',
+  '⪕': '\\eqslantless',
+  '⊕': '\\oplus',
+  '±': '\\pm',
+  '∙': '\\bullet',
+  '∀': '\\forall',
+  '∁': '\\complement',
+  '∴': '\\therefore',
+  '∅': '\\varnothing',
+  '∃': '\\exists',
+  '⊂': '\\subset',
+  '∵': '\\because',
+  '⊃': '\\supset',
+  '↦': '\\mapsto',
+  '∄': '\\nexists',
+  '∣': '\\mid',
+  '→': '\\to',
+  '⟹': '\\implies',
+  '∈': '\\in',
+  '∧': '\\land',
+  '←': '\\gets',
+  '⟸': '\\impliedby',
+  '∨': '\\lor',
+  '↔': '\\leftrightarrow',
+  '⟺': '\\iff',
+  '∋': '\\ni',
+  '¬': '\\neg',
+})
+
+function setTex(val = '') {
+  tex.value = val
+}
+
+function getTex() {
+  return tex.value
+}
+
+defineExpose({setTex, getTex})
+
+</script>
+
+<template>
+  <div style="margin-bottom: 10px">
+    <n-popover>
+      <template #trigger>
+        <n-button size="small" type="primary" secondary>希腊字母</n-button>
+      </template>
+      <n-grid :y-gap="5" :cols="12">
+        <n-grid-item class="item-hover" v-for="(val, key, i) in letters" @click="addTex(val)">
+          {{ key }}
+        </n-grid-item>
+      </n-grid>
+    </n-popover>
+    <n-popover>
+      <template #trigger>
+        <n-button size="small" type="primary"  secondary style="margin-left: 10px">逻辑符号</n-button>
+      </template>
+      <n-grid :y-gap="5" :cols="12">
+        <n-grid-item class="item-hover" v-for="(val, key, i) in logic" @click="addTex(val)">
+          {{ key }}
+        </n-grid-item>
+      </n-grid>
+    </n-popover>
+  </div>
+  <div class="texbox">
+    <textarea ref="TEXAREA" v-model="tex" class="texarea" placeholder="输入TeX公式"/>
+  </div>
+  <div class="preview-title">预览</div>
+  <div class="preview">
+    <p ref="box"></p>
+  </div>
+</template>
+
+<style scoped>
+.item-hover {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 17px;
+  color: v-bind(themeVars.textColor3);
+  width: 30px;
+  height: 30px;
+}
+
+.item-hover:hover {
+  background: #aaa;
+}
+
+.texbox {
+  box-sizing: border-box;
+  border-radius: 3px;
+  height: 200px;
+  overflow: hidden;
+}
+
+.texarea {
+  width: 100%;
+  height: 100%;
+  outline: none;
+  resize: none;
+  background: v-bind(themeVars.inputColor);
+  padding: 10px;
+  box-sizing: border-box;
+  color: v-bind(themeVars.textColor1);
+  border-color: v-bind(themeVars.borderColor);
+  border-radius: 3px;
+}
+.texarea:focus{
+  border-color: v-bind(themeVars.primaryColor) !important;
+  box-shadow: 0 0 0 2px rgba(24, 160, 88, 0.2);
+}
+
+.preview-title{
+  font-size: 12px;
+  margin-top: 20px;
+  color: v-bind(themeVars.textColor3);
+}
+
+.preview {
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid v-bind(themeVars.textColor3);
+  border-radius: 3px;
+  min-height: 80px;
+}
+</style>
