308830d40b1ec20dcb5c22fa4b71b42df9fd1319
pumelotea <535553297@qq.com>
2022-09-01 22:36:41 +0800

    feat: 数学公式支持二次编辑

--- src/components/tiptap/components/HbTiptapMathComponent.vue
+++ src/components/tiptap/components/HbTiptapMathComponent.vue
@@ -1,24 +1,44 @@
 <script setup>
-import { NodeViewContent, nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3'
+import {NodeViewContent, nodeViewProps, NodeViewWrapper} from '@tiptap/vue-3'
 import 'katex/dist/katex.css'
 import katex from 'katex'
-import { NPopover } from 'naive-ui'
+import {NPopover} from 'naive-ui'
 import HbTiptapMenuItem from "@/components/tiptap/components/HbTiptapMenuItem"
 
-import { onMounted, ref } from 'vue'
+import {onMounted, ref, watch} from 'vue'
+import HbTiptapMath from "@/components/tiptap/components/HbTiptapMath";
+
 const props = defineProps(nodeViewProps)
 
 const box = ref(null)
 
-function init () {
+function init() {
   katex.render(props.node.attrs.tex, box.value, {
     throwOnError: false
   })
 }
 
+watch(() => props.node.attrs.tex, () => {
+  init()
+})
+
 onMounted(() => {
   init()
 })
+
+const HTM = ref(null)
+
+function handleOpenMath() {
+  HTM.value.open(props.node.attrs.tex)
+}
+
+function onok(val) {
+  if (!val) {
+    props.editor.commands.deleteSelection()
+    return
+  }
+  props.updateAttributes({tex: val})
+}
 
 </script>
 <template>
@@ -26,16 +46,16 @@ onMounted(() => {
     <n-popover placement="bottom-end" :showArrow="false" trigger="click" style="padding: 0">
       <template #trigger>
         <p
-          ref="box"
-          :class="{'math-selected':props.selected}"
+            ref="box"
+            :class="{'math-selected':props.selected}"
         />
       </template>
       <div class="image-actions">
         <div class="action-item">
           <hb-tiptap-menu-item
               icon="settings-line"
-              title="修改图片"
-              :action="()=>{}"
+              title="修改公式"
+              :action="handleOpenMath"
               :is-active="() => {}"
           ></hb-tiptap-menu-item>
         </div>
@@ -43,13 +63,17 @@ onMounted(() => {
           <hb-tiptap-menu-item
               icon="delete-bin-2-line"
               title="删除"
-              :action="()=>{}"
+              :action="()=>props.editor.commands.deleteSelection()"
               :is-active="() => {}"
           ></hb-tiptap-menu-item>
         </div>
       </div>
     </n-popover>
   </node-view-wrapper>
+  <hb-tiptap-math
+      ref="HTM"
+      @ok="onok"
+  ></hb-tiptap-math>
 </template>
 
 <style scoped>
@@ -64,22 +88,23 @@ onMounted(() => {
   padding: 0 5px;
 }
 
-.math-selected{
-  background:rgb(0 150 255 / 32%);
+.math-selected {
+  background: rgb(0 150 255 / 32%);
 }
 
-.image-actions{
+.image-actions {
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 2px;
 }
-.action-item{
+
+.action-item {
   display: flex;
   align-items: center;
 }
 
-.action-item + .action-item{
+.action-item + .action-item {
   margin-top: 2px;
 }
 </style>
